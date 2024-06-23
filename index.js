feb00030844458901846f8fadd914c28b94758b2
Md Mizanur Rahman <itsrmizanur@gmail.com>
2024-06-23 19:34:59 +0600

    1. done

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1) Write a function named ‘calculateSum’ that takes two arguments and returns their sum.
+
+function calculateSum ( a, b ) {
+    return a+b;
+}
+
+console.log("1. Sum is: " + calculateSum(2, 4));
+
+
